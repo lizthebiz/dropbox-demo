ba15bf5d3da573950504c6880a3b0ea80d7a485f
lizthebiz <lizdalay@gmail.com>
2015-10-10 01:40:19 -0700

    Welcome screens + most of Create Account
    Main tasks learned: figured out scroll view for the welcome screens,
    performed manual segue for the first Create Account screen, and added
    button actions in code (tapping button = going back to a previous View
    Controller).

---
+++ dropbox/CreateAccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateAccountViewController.swift
+//  dropbox
+//
+//  Created by Liz Dalay on 10/10/15.
+//  Copyright © 2015 Liz Dalay. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onButton(sender: AnyObject) {
+        navigationController!.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ dropbox/welcomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  welcomeViewController.swift
+//  dropbox
+//
+//  Created by Liz Dalay on 10/9/15.
+//  Copyright © 2015 Liz Dalay. All rights reserved.
+//
+
+import UIKit
+
+class welcomeViewController: UIViewController {
+
+    @IBOutlet weak var welcomeScrollView: UIScrollView!
+    
+    @IBOutlet weak var welcomeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        welcomeScrollView.contentSize = welcomeImageView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
